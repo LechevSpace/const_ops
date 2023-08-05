e7b847d8e6be4db097abae701a34c599d780b1d7
hannses <hannes.hofmuth@gmail.com>
2023-08-05 10:44:05 +0200

    adds f32 and f64 impl for PartialEq

--- src/impls/partial_eq.rs
+++ src/impls/partial_eq.rs
@@ -10,4 +10,4 @@ macro_rules! partialeq_impl {
     )*)
 }
 
-partialeq_impl! { bool usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 }
+partialeq_impl! { bool usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64 }
